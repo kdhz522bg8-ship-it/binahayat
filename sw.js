f2251974fb9624563761ef7d7abe65c586423f7e
binahayat <kdhz522bg8@privaterelay.appleid.com>
2026-08-14 15:16:48 +0330

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>self.clients.claim());self.addEventListener("push",e=>{const d=e.data?e.data.json():{title:"∞ املاک بینهایت",body:"یک کار مهم داری"};e.waitUntil(self.registration.showNotification(d.title,{body:d.body,icon:"icon.svg",badge:"icon.svg"}))});
