d28672ee8e3e67134a868eb9cd5d339735344c43
binahayat <kdhz522bg8@privaterelay.appleid.com>
2026-08-24 18:31:57 +0330

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE="binahayat-v1001";
-const ASSETS=["./","./index.html","./manifest.webmanifest"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{if(r.ok)caches.open(CACHE).then(c=>c.put(e.request,r.clone())).catch(()=>{});return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
